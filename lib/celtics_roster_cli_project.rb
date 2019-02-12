f9c7a204a489ee138e27076bd8c261202bad18c3
Tom Holmes <tholmes59@gmail.com>
2019-02-12 19:18:08 +0000

    first commit

---
+++ lib/celtics_roster_cli_project.rb
@@ -0,0 +1,6 @@
+require "celtics_roster_cli_project/version"
+
+module CelticsRosterCliProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/celtics_roster_cli_project/version.rb
@@ -0,0 +1,3 @@
+module CelticsRosterCliProject
+  VERSION = "0.1.0"
+end
