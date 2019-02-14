70ff9844d84eeff6bac5bfc47d38e1c3530b0dab
Tom Holmes <tholmes59@gmail.com>
2019-02-14 22:07:28 +0000

    added libraries to lib

--- lib/celtics_roster_cli_project.rb
+++ lib/celtics_roster_cli_project.rb
@@ -1,4 +1,10 @@
-require "celtics_roster_cli_project/version"
+require "./celtics_roster_cli_project/version"
+require "./celtics_roster_cli_project/cli"
+require "./celtics_roster_cli_project/player"
+require "./celtics_roster_cli_project/scraper"
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
 
 module CelticsRosterCliProject
   class Error < StandardError; end
