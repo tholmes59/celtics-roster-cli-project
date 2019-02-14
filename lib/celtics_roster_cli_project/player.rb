b380ec46f881eab9bcb571096e1f8ee3c73e473b
Tom Holmes <tholmes59@gmail.com>
2019-02-14 18:05:04 +0000

    add initialize method to player

--- lib/celtics_roster_cli_project/player.rb
+++ lib/celtics_roster_cli_project/player.rb
@@ -2,6 +2,18 @@ class CelticsRoster::Player
   
   attr_accessor :name, :position, :url, :body, :age, :hometown, :school, :experience, :points, :rebounds, :assists, :field_goal
   
+  @@all = []
+  
+  def initialize(name = nil, position = nil, url = nil)
+    @name = name
+    @position = position
+    @url = url
+    @@all << all
+  end 
+  
+  def self.all
+    @@all
+  end
   
   
 end
