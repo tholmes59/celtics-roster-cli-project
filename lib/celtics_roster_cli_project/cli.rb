0a444d396f163c58182d32685ee7748b86e3d517
Tom Holmes <tholmes59@gmail.com>
2019-02-14 17:45:29 +0000

    updated CLI class

--- lib/celtics_roster_cli_project/cli.rb
+++ lib/celtics_roster_cli_project/cli.rb
@@ -1,5 +1,32 @@
 class CelticsRoster::CLI 
   
+  def call 
+    #initiate the scrape
+    #call a method to print a list of players
+    #call a method to display individual players
+    #goodbye method
+  end 
+  
+  def list_players
+    
+   # puts "1. Playername - position"
+  end
+  
+  def display_player
+    
+    
+    #puts height/weight
+    #puts age 
+    #puts hometown
+    #puts school
+    #puts years of experience
+    
+    # stats 
+    #puts points per game 
+    #puts rebounds per game
+    #puts assists per game
+    #puts field goal percentage
+  end
   
   
 end
