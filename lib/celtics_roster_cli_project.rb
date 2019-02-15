20f2d9d48e39f9db0790e3f9fa5ed0a0362a0bec
Tom Holmes <tholmes59@gmail.com>
2019-02-15 01:30:51 +0000

    populate config environment

--- lib/celtics_roster_cli_project.rb
+++ lib/celtics_roster_cli_project.rb
@@ -2,11 +2,5 @@ module CelticsRoster
 end
 
 require_relative '../config/environment'
-require "./celtics_roster_cli_project/version"
-require "./celtics_roster_cli_project/cli"
-require "./celtics_roster_cli_project/player"
-require "./celtics_roster_cli_project/scraper"
-require 'open-uri'
-require 'nokogiri'
-require 'pry'
+
 
