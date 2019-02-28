27363fffb4916e3f7a1eedf76e357c6c07695498
Tom Holmes <tholmes59@gmail.com>
2019-02-28 21:40:18 +0000

    Move player detail methods from Player to Scraper class

--- lib/celtics_roster_cli_project/scraper.rb
+++ lib/celtics_roster_cli_project/scraper.rb
@@ -18,4 +18,48 @@ class CelticsRosterCliProject::Scraper
       )
   end
   
+  def page
+    @page ||= Nokogiri::HTML(open(self.url))
+  end 
+  
+  def body
+    @body ||= page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(2)").text
+  end 
+  
+  def age
+    @age ||= page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(6)").text
+  end 
+  
+  def hometown
+    @hometown ||= page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(8)").text
+  end
+  
+  def school
+    @school ||= page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(12)").text
+  end
+  
+  def experience
+    @experience ||= page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(14)").text
+  end
+  
+  def points
+    @points ||= page.search("div.stats.fRight.stdMargin tr:nth-child(3) td:nth-child(3)").text
+  end
+  
+  def rebounds
+    @rebounds ||= page.search("div.stats.fRight.stdMargin tr:nth-child(3) td:nth-child(4)").text
+  end
+  
+  def assists
+    @assists ||= page.search("div.stats.fRight.stdMargin tr:nth-child(3) td:nth-child(5)").text
+  end
+  
+  def field_goal
+    @field_goal ||= page.search("div.stats.fRight.stdMargin tr:nth-child(3) td:nth-child(6)").text
+  end
+  
+  def position
+    @position ||= page.search("div.row.row-no-pad h1 span:nth-child(3)").text
+  end
+  
 end
