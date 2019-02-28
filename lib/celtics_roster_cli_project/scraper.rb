7f6ec0613749b4b7c60f6a3334e11e10e070bfb4
Tom Holmes <tholmes59@gmail.com>
2019-02-28 21:29:04 +0000

    Combine get_page and scrape_roster methods in Scraper class

--- lib/celtics_roster_cli_project/scraper.rb
+++ lib/celtics_roster_cli_project/scraper.rb
@@ -1,11 +1,8 @@
 class CelticsRosterCliProject::Scraper
   
-  def get_page
-    Nokogiri::HTML(open("https://www.cbssports.com/nba/teams/BOS/boston-celtics/roster/"))
-  end 
-  
   def scrape_roster
-    self.get_page.search("div.TableBase-overflow tbody tr")
+    doc = Nokogiri::HTML(open("https://www.cbssports.com/nba/teams/BOS/boston-celtics/roster/"))
+    doc.search("div.TableBase-overflow tbody tr")
   end 
   
   def create_the_players
