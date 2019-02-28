0eab3839b4d74ed8874dde6dbe6f4d41d0580d57
Tom Holmes <tholmes59@gmail.com>
2019-02-28 22:00:52 +0000

    Create scrape_player_details method to replace individual player stats methods

--- lib/celtics_roster_cli_project/scraper.rb
+++ lib/celtics_roster_cli_project/scraper.rb
@@ -18,48 +18,62 @@ class CelticsRosterCliProject::Scraper
       )
   end
   
-  def page
-    @page ||= Nokogiri::HTML(open(self.url))
-  end 
+  # def page
+  #   @page ||= Nokogiri::HTML(open(self.url))
+  # end 
   
-  def body
-    @body ||= page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(2)").text
-  end 
+  # def body
+  #   @body ||= page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(2)").text
+  # end 
   
-  def age
-    @age ||= page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(6)").text
-  end 
+  # def age
+  #   @age ||= page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(6)").text
+  # end 
   
-  def hometown
-    @hometown ||= page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(8)").text
-  end
+  # def hometown
+  #   @hometown ||= page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(8)").text
+  # end
   
-  def school
-    @school ||= page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(12)").text
-  end
+  # def school
+  #   @school ||= page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(12)").text
+  # end
   
-  def experience
-    @experience ||= page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(14)").text
-  end
+  # def experience
+  #   @experience ||= page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(14)").text
+  # end
   
-  def points
-    @points ||= page.search("div.stats.fRight.stdMargin tr:nth-child(3) td:nth-child(3)").text
-  end
+  # def points
+  #   @points ||= page.search("div.stats.fRight.stdMargin tr:nth-child(3) td:nth-child(3)").text
+  # end
   
-  def rebounds
-    @rebounds ||= page.search("div.stats.fRight.stdMargin tr:nth-child(3) td:nth-child(4)").text
-  end
+  # def rebounds
+  #   @rebounds ||= page.search("div.stats.fRight.stdMargin tr:nth-child(3) td:nth-child(4)").text
+  # end
   
-  def assists
-    @assists ||= page.search("div.stats.fRight.stdMargin tr:nth-child(3) td:nth-child(5)").text
-  end
+  # def assists
+  #   @assists ||= page.search("div.stats.fRight.stdMargin tr:nth-child(3) td:nth-child(5)").text
+  # end
   
-  def field_goal
-    @field_goal ||= page.search("div.stats.fRight.stdMargin tr:nth-child(3) td:nth-child(6)").text
-  end
+  # def field_goal
+  #   @field_goal ||= page.search("div.stats.fRight.stdMargin tr:nth-child(3) td:nth-child(6)").text
+  # end
   
-  def position
-    @position ||= page.search("div.row.row-no-pad h1 span:nth-child(3)").text
+  # def position
+  #   @position ||= page.search("div.row.row-no-pad h1 span:nth-child(3)").text
+  # end
+  
+  def scrape_player_details(player)
+    page = Nokogiri::HTML(open(player.url))
+    player.body = page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(2)").text
+    player.age = page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(6)").text
+    player.hometown = page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(8)").text
+    player.school = page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(12)").text
+    player.experience = page.search("div.featureComponent.stdPad.mBottom10 dl dd:nth-child(14)").text
+    player.points = page.search("div.stats.fRight.stdMargin tr:nth-child(3) td:nth-child(3)").text
+    player.rebounds = page.search("div.stats.fRight.stdMargin tr:nth-child(3) td:nth-child(4)").text
+    player.assists = page.search("div.stats.fRight.stdMargin tr:nth-child(3) td:nth-child(5)").text
+    player.field_goal = page.search("div.stats.fRight.stdMargin tr:nth-child(3) td:nth-child(6)").text
+    player.position = page.search("div.row.row-no-pad h1 span:nth-child(3)").text
   end
   
 end
